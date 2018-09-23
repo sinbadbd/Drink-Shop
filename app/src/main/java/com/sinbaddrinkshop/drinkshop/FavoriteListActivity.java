872d3868c156cd39876187887a6345a7147a5fc5
imran <imufun1@gmail.com>
2018-09-23 08:57:41 +0600

    add

---
+++ app/src/main/java/com/sinbaddrinkshop/drinkshop/FavoriteListActivity.java
@@ -0,0 +1,13 @@
+package com.sinbaddrinkshop.drinkshop;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class FavoriteListActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_list);
+    }
+}
